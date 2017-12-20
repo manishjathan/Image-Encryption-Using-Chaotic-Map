788b8764b517780d572e5b3cb79c253db0b39669
Manish Jathan <30714579+manishjathan@users.noreply.github.com>
2017-12-20 07:04:53 +0530

    Generates an image after pixel shuffling
    Makes use of Arnold's Cat Map for pixel Shuffling

--- generateArnoldMap.py
+++ generateArnoldMap.py
@@ -1,7 +1,6 @@
+from PIL import Image
 import createImageMatrix as cim
 import numpy as np
-from scipy.linalg import toeplitz
-
 
 def multiplyMatrix(A,B,res):
     if len(A[0]) == len(B):
@@ -27,35 +26,75 @@ def matrixModulo(inputMatrix,n):
             newMatrix = [newRow]
     return newMatrix
 
-def findMatch(res,inputMatrix):
-    res1 = np.zeros(shape = (175,175))
-    res1 = matrixModulo(multiplyMatrix(res,inputMatrix,res1),175)
+def genArnoldMap(res,inputMatrix):
+    map = {}
+    res1 = np.zeros(shape = (2,1))
+    res1 = matrixModulo(multiplyMatrix(res,inputMatrix,res1),50)
     count = 0
-    while (res1 != inputMatrix) and (count < 1000):
-        res2 = np.zeros(shape = (175,175))
-        res1 = matrixModulo(multiplyMatrix(res,res1,res2),175)
+    while (res1 != inputMatrix) and (count < 10):
+        res2 = np.zeros(shape = (2,1))
+        res1 = matrixModulo(multiplyMatrix(res,res1,res2),50)
         count += 1
-        print(count)
-    print("Number of Iterations : ",count)
-
-
+    newtuple = (res1[0][0],res1[1][0])
+    actualtuple = (inputMatrix[0][0],inputMatrix[1][0])
+    map[actualtuple] = newtuple
+    return map
 
 def driverProgram():
-    n = 175
+    n = 2
+    mapList = []
     UTM  = np.triu(np.ones((n,n),dtype = int),0)
     LTM =  np.tril(np.ones((n,n),dtype = int),0)
     RM = np.zeros(shape = (n,n))
     res = multiplyMatrix(LTM,UTM,RM)
     print(res)
-    bluePixelMatrix = cim.getImageMatrix(2)
-    findMatch(res,bluePixelMatrix)
+    for i in range(50):
+        for j in range(50):
+            inputMatrix = [[i],[j]]
+            map = genArnoldMap(res,inputMatrix)
+            try:
+                mapList.append(map)
+            except:
+                mapList = [map]
+    print(mapList)
+    return mapList
 
-driverProgram()
+def createArnoldCatImage():
+    arnoldImageMatrix = []
+    #Creating a zero matrix for arnold Cat map
+    for i in range(50):
+        row = []
+        for j in range(50):
+            try:
+                row.append((0,0,0))
+            except:
+                row = [(0,0,0)]
+        try:
+            arnoldImageMatrix.append(row)
+        except:
+            arnoldImageMatrix = [row]
 
-#redPixelMatrix = cim.getImageMatrix(0)
-#greenPixelMatrix = cim.getImageMatrix(1)
-#bluePixelMatrix = cim.getImageMatrix(2)
+    #Inserting pixel values into arnold Cat Image Matrix
+    for map in mapList:
+        for keys in map.keys():
+            print(imageMatrix[keys[0]][keys[1]],int(map[keys][0]),int(map[keys][1]))
+            arnoldImageMatrix[int(map[keys][0])][int(map[keys][1])] = (imageMatrix[keys[0]][keys[1]])
 
+    print(arnoldImageMatrix)
+    file = open("ArnoldImageMatrix.csv", "w")
+    file.write(str(arnoldImageMatrix))
+    file.close()
+
+    im = Image.new("RGB", (50, 50))
+    pix = im.load()
+    for x in range(50):
+        for y in range(50):
+            pix[x, y] = arnoldImageMatrix[x][y]
+    im.save("test.png", "PNG")
+
+mapList = driverProgram()
+imageMatrix = cim.getImageMatrix()
+createArnoldCatImage()
 
 
 
