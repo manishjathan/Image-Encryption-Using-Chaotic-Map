48f57cb56c5b8380de336e28780398745c74ce44
Manish Jathan <30714579+manishjathan@users.noreply.github.com>
2018-01-02 23:24:19 +0530

    Add files via upload

--- Image Encryption UI.py
+++ Image Encryption UI.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import filedialog
 import os
+import generateArnoldMap as gam
+import ImageTransformation as iT
 from PIL import ImageTk, Image
 
 def choose_File():
@@ -15,34 +17,30 @@ def performArnoldShuffle():
     print(fileName)
     absFilePath = os.path.abspath(fileName)
     print(absFilePath)
-    entry2.insert(0,absFilePath)
+
+    mapList = gam.driverProgram()
+    imageMatrix = gam.cim.getImageMatrix(absFilePath)
+    resImage = gam.createArnoldCatImage(imageMatrix,mapList)
+
+    entry2.insert(0,resImage)
 
 
 def performHenonManipulation():
     filename = entry1.get()
-    print(filename)
+    resImage = iT.pixelManipulation(512, filename)
+    entry3.insert(0,resImage)
+    #print(filename)
 
 def openFile():
-    #root.destroy()
-    window = Tk()
-    window.title("Join")
+    window = Toplevel(root)
+    window.title("Henon Map")
     window.geometry("600x600")
-    window.configure(background='grey')
-
-    path = "lena_gray.bmp"
-    fullpath = os.path.abspath(path)
-    print(fullpath)
-    # Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-    img = ImageTk.PhotoImage(Image.open(fullpath))
-
-    # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+    path = entry3.get()
+    img = ImageTk.PhotoImage(Image.open(path))
     panel = Label(window, image=img)
-
-    # The Pack geometry manager packs widgets in rows or columns.
     panel.pack(side="bottom", fill="both", expand="yes")
-
-    # Start the GUI
     window.mainloop()
+
 #from tkFileDialog import askopenfilename
 
 root =Tk()
@@ -59,14 +57,16 @@ label_1 = Label(topFrame, text ="Image to be Encrypted : ",width = 125)
 entry1 = Entry(topFrame,width =100)
 button1 = Button(topFrame, text = "Select Image",command = choose_File)
 
+
 button2 = Button(middleFrame, text = "Generate Arnold Cat Map",command = performArnoldShuffle)
 entry2 = Entry(middleFrame,width =80)
 button3 = Button(middleFrame, text="Open Image",command = openFile)
 
-
 button4 = Button(bottomFrame, text="Generate Henon Map",command = performHenonManipulation)
 entry3 = Entry(bottomFrame,width =80)
-button5 = Button(bottomFrame, text="Open Image")
+#entry3.insert(END,'default text')
+#path = entry3.get()
+button5 = Button(bottomFrame, text="Open Image",command = openFile)
 
 label_1.pack(side = TOP)
 entry1.pack(side = TOP)
